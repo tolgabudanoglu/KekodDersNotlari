dc465120a5eea2d60e7fc597b2426f389de44405
tolga budanoğlu <budanoglu.tolga@gmail.com>
2024-02-13 22:50:01 +0300

    val neden immutable değilde read only eklendi

--- HelloWord/src/main/kotlin/Main.kt
+++ HelloWord/src/main/kotlin/Main.kt
@@ -11,12 +11,37 @@ fun main() {
     var isStudent:Boolean = false
 
 
+
 }
 
 fun main(args: Array<String>) {
     for (lang in args) println(lang)
+
+    A().calculate()
 }
 class A(){
+
+    //val neden immutable değilde read only açıklaması
+    var age2 = 25
+    var age3 = 4
+    val age4:Int
+        get(){
+            return age2*age3
+        }
+
+    fun calculate(){
+        val a = A()
+        a.age2 = 10
+        a.age3 = 5
+        println(a.age4)
+
+        a.age2 = 12
+        a.age3 = 4
+        println(a.age4)
+
+    }
+
+
 
     var isMale = true
         private set
