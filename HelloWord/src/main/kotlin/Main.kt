a2c909b5507ad1070df53662892145902905eb03
tolga budanoğlu <budanoglu.tolga@gmail.com>
2024-02-13 21:24:59 +0300

    değişken tanımlama , var val fark eklendi

--- HelloWord/src/main/kotlin/Main.kt
+++ HelloWord/src/main/kotlin/Main.kt
@@ -1,8 +1,27 @@
-fun main(){
+fun main() {
     println("hello worlda")
+    val name:String = "tolga"
+    val age = 34 // type inference = değişken tipini kendi anlaması = tip çıkarımı = bir değişkene eşittir ile verdiysek ide nin değişken tipini kendi çıkarması
+
+    val surname:String
+
+    surname = readLine()!! // kullanıcıdan isteme
+
+    var isMale = true
+    var isStudent:Boolean = false
+
+
 }
 
 fun main(args: Array<String>) {
-    for (lang in args)
-        println(lang)
+    for (lang in args) println(lang)
+}
+class A(){
+
+    var isMale = true
+        private set
+
+
+
+    var isStudent:Boolean = false
 }
