f4be14d3e885e1dcad2b964392404b236e4cabfc
tolga budanoğlu <budanoglu.tolga@gmail.com>
2024-03-20 18:22:02 +0300

    crossinline kodları eklendi

--- KotlinLessons/src/main/kotlin/Functions/InlineNoInlineCroosInline.kt
+++ KotlinLessons/src/main/kotlin/Functions/InlineNoInlineCroosInline.kt
@@ -2,6 +2,30 @@ package Functions
 
 fun main() {
 
+    //non locking return
+    println("start")
+    val list = listOf(1,2,3,4,5,6,7)
+    list.forEach{
+        if (it == 5){
+            return
+        }
+        println(it)
+    }
+    println("end")
+
+
+
+    println("start")
+    val list2 = listOf(1,2,3,4,5,6,7)
+    list2.forEach label@{
+        if (it == 5){
+            return@label
+        }
+        println(it)
+    }
+    println("end")
+
+
     runAndPrint {
         println(it)
     }
@@ -31,3 +55,4 @@ inline fun runAndPrint2(run: (String) -> Unit, noinline logger :(String) -> Unit
     logger("start")
 
 }
+
