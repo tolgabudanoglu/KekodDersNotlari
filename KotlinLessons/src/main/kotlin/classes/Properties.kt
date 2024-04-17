038c9956bf2e7cdd5d3cfc75b4907ab9b91f2ddf
tolga budanoğlu <budanoglu.tolga@gmail.com>
2024-04-17 18:54:51 +0300

    properties kodları

--- KotlinLessons/src/main/kotlin/classes/Properties.kt
+++ KotlinLessons/src/main/kotlin/classes/Properties.kt
@@ -3,3 +3,18 @@ package classes
 fun main() {
     
 }
+
+class Rectangle2(val width: Int, val height: Int) {
+    val area: Int
+        get() = this.width * this.height
+}
+//var değişkeni val değişkene dönüştürme
+var setterVisibility: String = "abc"
+    private set // the setter is private and has the default implementation
+
+var setterWithAnnotation: Any? = null
+     set // annotate the setter with Inject
+
+// backing fieldi yok çünkü değeri yok
+val isEmpty: Boolean
+    get() = this.size == 0
