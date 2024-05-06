0bb8b3ae2e43673ab5ec3061da02fb29b6ae096f
tolga budanoğlu <budanoglu.tolga@gmail.com>
2024-05-06 21:27:32 +0300

    enum class kodları

--- KotlinLessons/src/main/kotlin/classes/enumclasses/Team.kt
+++ KotlinLessons/src/main/kotlin/classes/enumclasses/Team.kt
@@ -1,4 +1,34 @@
 package classes.enumclasses
 
-enum class Team {
+enum class Team private constructor(val starCount:Int){
+    GENERBAHCE(3) {
+        override fun boo() {
+            TODO("Not yet implemented")
+        }
+    },GALATASARAY(4) {
+        override fun boo() {
+            TODO("Not yet implemented")
+        }
+    },BESIKTAS(3) {
+        override fun boo() {
+            TODO("Not yet implemented")
+        }
+    };
+
+    init{
+
+    }
+
+
+    abstract fun boo()
+}
+
+fun main(){
+
+    Team.values() //entries aynı
+    Team.valueOf("GALATASARAY")
+    Team.BESIKTAS.starCount
+
+    println(Team.GENERBAHCE.name) //enum sabitinin ismi
+    println(Team.GENERBAHCE.ordinal) // sabitin sırasını
 }
