acb85d5a4d0edfb04b6858494f87b9f53924a2de
tolga budanoğlu <budanoglu.tolga@gmail.com>
2024-05-09 21:35:44 +0300

    generics kodalrı

--- KotlinLessons/src/main/kotlin/classes/generics/Generics.kt
+++ KotlinLessons/src/main/kotlin/classes/generics/Generics.kt
@@ -6,6 +6,13 @@ fun main() {
 
     homeFragment.call()
     dashboardFragment.call()
+
+    val box :Box<Int> = Box(1)
+    val boz3 =  Box("box") // type inference
+}
+
+class Box<T>(t:T){
+    var value =t
 }
 abstract class BaseFragment{
     fun <T> printNParam(param: T){
@@ -23,4 +30,29 @@ class DashboardFragment: BaseFragment(){
     fun call(){
         printNParam("gkag")
     }
+}
+
+/*
+*
+*  declaration site variance
+*
+*
+ */
+// out co - variance kullanımlar için (kendisi ve subtypeları (alt sınıf) geçerlı olsun istiyortsak
+interface Source<out T>{
+    fun next():T
+}
+
+fun demo(strs:Source<String>){
+    val objects:Source<Any> = strs
+}
+
+//in contra variance kullanım (kendisi ve superType(üst sınıf) geçerli)
+interface Comparabble<in T>{
+    operator fun compareTo(other:T):Int
+}
+
+fun demo(x:Comparabble<Number>){
+
+    val y:Comparabble<Double> = x
 }
