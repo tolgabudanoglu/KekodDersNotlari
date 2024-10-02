c87eb7bc6f5b03924e5c8842c2e4001fa0d142c8
tolga budanoğlu <budanoglu.tolga@gmail.com>
2024-10-02 19:00:18 +0300

    nav graph route

--- AndroidProjeler/AppNavigation/app/src/main/java/com/example/appnavigation/DashboardFragment.kt
+++ AndroidProjeler/AppNavigation/app/src/main/java/com/example/appnavigation/DashboardFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 
 class DashboardFragment : Fragment() {
@@ -14,8 +16,18 @@ class DashboardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_dashboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //route
+        val btnOpenProfile = view.findViewById<Button>(R.id.btnOpenProfile) // fragmentte findViewbyId yok
+        btnOpenProfile.setOnClickListener {
+            findNavController().navigate(R.id.action_dashboardFragment_to_profileFragment)
+        }
     }
 
 
--- AndroidProjeler/AppNavigation/app/src/main/java/com/example/appnavigation/MainActivity.kt
+++ AndroidProjeler/AppNavigation/app/src/main/java/com/example/appnavigation/MainActivity.kt
@@ -12,37 +12,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //getNavControllerViaFragment()
+        getNavControllerViaFragment()
         //getNavControllerViaView()
        // getNavControllerViaFragment2()
        //getNavControllerViaView2() //çalışmaz on create de
 
+        //getNavControllerViaFragment3()
+       // getNavControllerViaView3()//çalışmaz on create de
     }
 
-    // supportFragmentManager üzerinde ikisnde de çalışır
+    // birinci yol en az hatalı yöntem
     fun getNavControllerViaFragment(){
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
     }
-
-    fun getNavControllerViaView(){
-        val navHostFragmentView = supportFragmentManager.findFragmentById(R.id.navHostFragmentView) as NavHostFragment
-        val navControllerView = navHostFragmentView.navController
-    }
-
+    //ikinci yol
     fun getNavControllerViaFragment2(){
         val navHostFragment = findViewById<View>(R.id.navHostFragment)
         val navController = navHostFragment.findNavController() //extension fonk
     }
+    //3. yol
+    fun getNavControllerViaFragment3(){
+        findNavController(R.id.navHostFragment)
+    }
+    // birinci yol en az hatalı yöntem bunu tercih et
+    fun getNavControllerViaView(){
+        val navHostFragmentView = supportFragmentManager.findFragmentById(R.id.navHostFragmentView) as NavHostFragment
+        val navControllerView = navHostFragmentView.navController
+    }
+    //ikinci yol
     //fragment contaienr viewi findViewById ile çağrıırm yaparsak bunu onCreate de kullanmayız on resume da kullanabiliriz
     fun getNavControllerViaView2(){
         val navHostFragment = findViewById<View>(R.id.navHostFragmentView)
         val navController = navHostFragment.findNavController() //extension fonk
     }
+    //3. yol
+    // yine onCreate de çalışmaz
+    fun getNavControllerViaView3(){
+        findNavController(R.id.navHostFragmentView)
+    }
 
     override fun onResume() {
         super.onResume()
         //getNavControllerViaView2() on resume da çalışır
+        //getNavControllerViaView3() on resume da çalışır
 
     }
 }
