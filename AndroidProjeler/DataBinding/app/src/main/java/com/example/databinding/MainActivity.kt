992c0e574694206cc979ca6111ae6bce5ab4d351
tolga budanoğlu <budanoglu.tolga@gmail.com>
2024-11-11 18:39:11 +0300

    data binding

--- AndroidProjeler/DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
+++ AndroidProjeler/DataBinding/app/src/main/java/com/example/databinding/MainActivity.kt
@@ -5,12 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding2 = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+
+        setContentView(binding.root)
+
+        binding.helloMessage = "Hello World!"
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
